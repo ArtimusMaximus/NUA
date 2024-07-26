54e811106b8d5a41efd235f38d6ef663e1271352
artimusmaximus <arthurmodymaniv@gmail.com>
2024-07-25 19:57:31 -0500

    changed DeviceSkeleton to display Loading... vs NUA, need to make skeleton display single skeleton item, OR display correct number of devices in skeleton format

--- src/components/Devices.jsx
+++ src/components/Devices.jsx
@@ -312,7 +312,7 @@ export default function Devices({ data, toggleReRender, handleRenderToggle, load
 
             <dialog ref={toggleLoadingDialogRef} className="modal">
                 <div className="flex items-center justify-center w-full h-full">
-                    <span className="loading loading-spinner w-1/2 h-1/2"></span>
+                    <span className="loading loading-spinner w-32 h-32"></span>
                 </div>
             </dialog>
         </>
--- src/components/skeletons/DevicesSkeleton.jsx
+++ src/components/skeletons/DevicesSkeleton.jsx
@@ -4,7 +4,7 @@ export default function DeviceSkeleton({ devices, loadingMacData })
         let arr=[];
         let length = 5;
         while(length > 0) {
-            arr.push('NUA')
+            arr.push('LOADING...')
             length -= 1;
         }
         return arr;
@@ -17,9 +17,9 @@ export default function DeviceSkeleton({ devices, loadingMacData })
                 skelArray.map((skeleton) => {
                     return (
                         <>
-                                <div className={`${loadingMacData ? 'skeleton w-full h-[60px]' : ''} grid-flow-row flex items-center justify-center mx-auto text-2xl gap-2 my-2 py-2`}>      {!devices && !loadingMacData && 'There is not data to display...'}
-                                <div className="flex flex-row items-center justify-center">
-                                    <span className="loading loading-spinner w-6 h-6"></span>{skeleton}
+                            <div className={`${loadingMacData ? 'skeleton w-full h-[60px]' : ''} grid-flow-row flex items-center justify-center mx-auto text-2xl gap-2 my-2 py-2`}>      {!devices && !loadingMacData && 'There is not data to display...'}
+                                <div className="flex flex-row items-center justify-center italic">
+                                    <span className="loading loading-spinner w-6 h-6 mr-2"></span>{skeleton}
                                 </div>
                             </div>
                         </>
