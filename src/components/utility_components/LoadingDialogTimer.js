18fda712520531edc9cd009912a0d3d5814bb2f7
artimusmaximus <arthurmodymaniv@gmail.com>
2024-08-26 20:42:52 -0500

    changed LoadingDialogTimer, removed asyncness, as it was not necessary

--- src/components/utility_components/LoadingDialogTimer.js
+++ src/components/utility_components/LoadingDialogTimer.js
@@ -1,5 +1,5 @@
-export async function loadingDialogTimer(dialogRef) {
-    await new Promise(res => setTimeout((res) => {
+export function loadingDialogTimer(dialogRef) {
+    setTimeout(() => {
         dialogRef.current.close();
-    }, 400));
+    }, 400);
 }
